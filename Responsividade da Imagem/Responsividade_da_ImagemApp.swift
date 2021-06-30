a7a92f5366e6d0942062fb78d27cc68fa4712a37
Rogerio <RoLucon@users.noreply.github.com>
2021-06-30 16:51:25 -0300

    Initial Commit

---
+++ Responsividade da Imagem/Responsividade_da_ImagemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Responsividade_da_ImagemApp.swift
+//  Responsividade da Imagem
+//
+//  Created by Rogerio Lucon on 30/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Responsividade_da_ImagemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
